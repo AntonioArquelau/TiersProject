97b8f9fe75e07caec04f3eded0dfde5a171cfe57
AntonioArquelau <antonio.arquelau@gmail.com>
2017-05-18 20:15:09 -0300

    comportamento dos inimigos

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	private Transform playerTranform;
+	private float velocity = 10;
+	private Vector3 teste;
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody> ();
+		playerTranform = GameObject.Find ("Player").GetComponent<Transform> ();
+		teste = playerTranform.position - transform.position;
+		rigid.velocity = teste/5;
+		Debug.Log (teste);	
+	}
+}
